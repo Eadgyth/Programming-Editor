2cc94d2213e9410f377056b7e85813102690bdef
Eadgyth <m.bussiek@web.de>
2017-04-04 21:33:58 +0200

    Finder class needed for current Coloring class

--- src/eg/utils/Finder.java
+++ src/eg/utils/Finder.java
@@ -5,37 +5,70 @@ package eg.utils;
  */
 public class Finder {
    
-   public static String currLine(String text, int pos) {
-      int indLastReturn = Finder.lastReturn(text, pos);
-      int indNextReturn = text.indexOf("\n", pos);
+   /**
+    * Returns the line which includes the specified position
+    *
+    * @param text  the entire text
+    * @param pos  the pos that includes the searched line
+    * @return  the line that includes '{@code pos}'
+    */
+   public static String lineAtPos(String text, int pos) {
+      int lastReturn = Finder.lastReturn(text, pos);
+      int nextReturn = Finder.nextReturn(text, pos);
 
-      if (indLastReturn != -1 && indNextReturn != -1) {
-         return text.substring(indLastReturn + 1, indNextReturn);
-      }
-      else if (indLastReturn != -1 && indNextReturn == -1) {
-         return text.substring(indLastReturn + 1);
-      }
-      else if (indLastReturn == -1 && indNextReturn != -1) {
-         return text.substring(0, indNextReturn);
+      if (lastReturn != -1) {
+         return text.substring(lastReturn + 1, nextReturn);
       }
       else {
-         return text;
+         return text.substring(0, nextReturn);
       }
    }
    
-   public static int lastReturn(String text, int pos) {
-       int indLastReturn = text.lastIndexOf("\n", pos);
-       if (indLastReturn == pos) {
-         indLastReturn = text.lastIndexOf("\n", pos - 1);
+   /**
+    * Returns the full lines of text which include the specified section
+    *
+    * @param  text  the entire text
+    * @param section  a section of the text
+    * @param pos  the position within the entire text where section starts
+    * @return the full lines of text which include the specified section
+    */
+   public static String allLinesAtPos(String text, String section, int pos) {
+      String[] sectionArr = section.split("\n");
+      String firstLine = Finder.lineAtPos(text, pos);  
+      sectionArr[0] = firstLine;
+      if (sectionArr.length > 1) {
+         String lastLine = Finder.lineAtPos(text, pos + section.length());
+         sectionArr[sectionArr.length - 1] = lastLine;
       }
-      return indLastReturn;
+      StringBuffer sb = new StringBuffer();
+      for (String s : sectionArr) {
+         sb.append(s);
+         sb.append("\n");
+      }
+      return sb.toString();
+   }
+   
+   public static int lastReturn(String text, int pos) {
+       int lastReturn = text.lastIndexOf("\n", pos);
+       if (lastReturn == pos) {
+         lastReturn = text.lastIndexOf("\n", pos - 1);
+      }
+      return lastReturn;
+   }
+   
+   public static int nextReturn(String text, int pos) {
+      int nextReturn = text.indexOf("\n", pos);
+      if (nextReturn == -1) {
+         nextReturn = text.length();
+      }
+      return nextReturn;
    }
 
-   public static int countMotif(String in, String motif) {
+   public static int countMotif(String text, String motif) {
       int count = 0;
       int index = 0;
       while (index != -1) {
-         index = in.indexOf(motif, index);
+         index = text.indexOf(motif, index);
          if (index != -1) {
             count++;
             index++;
