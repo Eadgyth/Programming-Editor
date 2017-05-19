63e8a6969d59686654a32ee751d7c6469d664af1
Eadgyth <m.bussiek@web.de>
2017-05-19 22:34:08 +0200

    textual changes in javadoc

--- src/eg/Languages.java
+++ src/eg/Languages.java
@@ -12,15 +12,28 @@ public enum Languages {
    
    private String display;
    
-   Languages(String display) {
+   private Languages(String display) {
       this.display = display;
    }
    
+   /**
+    * Returns the display value associated with a language
+    * constant
+    *
+    * @return  the display value for a language
+    */
    public String display() {
       return display;
    }
    
-   public static Languages langByDisplay(String display) {
+   /**
+    * Returns the language constant that is associated with the
+    * specified display value
+    *
+    * @param display  the display value for the language constant
+    * @return  the language constant with the specified display value
+    */
+   public static Languages languageByDisplay(String display) {
       Languages lang = null;
       for (Languages l : Languages.values()) {
          if (l.display().equals(display)) {
