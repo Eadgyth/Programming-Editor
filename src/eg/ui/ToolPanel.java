47d76beeb85438dad247e32090b5926aef989e93
Malte Bussiek <m.bussiek@web.de>
2017-11-04 23:28:07 +0100

    remove the title panel

--- src/eg/ui/ToolPanel.java
+++ src/eg/ui/ToolPanel.java
@@ -7,8 +7,8 @@ import java.awt.Dimension;
 import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
-import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JToolBar;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 
@@ -24,30 +24,27 @@ import eg.utils.UiComponents;
 public class ToolPanel {
    
    private final JPanel pnl = new JPanel(new BorderLayout());
-   private final JPanel titlePnl = new JPanel();
-   private final JLabel titleLb = new JLabel(" No function selected");
+   private final JPanel headPnl = new JPanel();
    private final JButton closeBt = new JButton(IconFiles.CLOSE_ICON);
    
    public ToolPanel() {
-      initPanel();
+      pnl.setBorder(Constants.GRAY_BORDER);
    }
    
    /**
-    * Adds a <code>Component</code> to this JPanel.
-    * <p> The panel has a <code>BorderLayout</code>. The title is
-    * shown in the north and the added component in the center. A
-    * previously added component is removed.
+    * Adds a <code>Component</code> to this tool panel.
+    * <p> The panel has a <code>BorderLayout</code> and gray border. The
+    * Component is added to the center. A previously added component is
+    * removed.
     *
     * @param c  the Component that is added
-    * @param title  the title for the tool
     */
-   public void addComponent(Component c, String title) {
+   public void addComponent(Component c) {
       BorderLayout layout = (BorderLayout) pnl.getLayout();
       Component cCenter = layout.getLayoutComponent(BorderLayout.CENTER);
       if (cCenter != null) {
          pnl.remove(cCenter);
       }
-      titleLb.setText(" " + title);
       if (c != null) {
          pnl.add(c, BorderLayout.CENTER);
       }
@@ -55,29 +52,8 @@ public class ToolPanel {
       pnl.repaint();
    }
    
-   public void closeAct(ActionListener al) {
-      closeBt.addActionListener(al);
-   }
-   
    public JPanel panel() {
       return pnl;
-   }
-   
-   private void initPanel() {
-      int lbHeight = 16;
-      Dimension dim = UiComponents.scaledDimension(0, lbHeight);
-      titlePnl.setPreferredSize(dim);
-      titlePnl.setLayout(new BoxLayout(titlePnl, BoxLayout.LINE_AXIS));
-      titlePnl.add(titleLb);
-      titleLb.setFont(Constants.SANSSERIF_PLAIN_9);
-      closeBt.setBorder(new EmptyBorder(3, 5, 3, 5));
-      closeBt.setContentAreaFilled(false);
-      closeBt.setToolTipText("Close function area");
-      closeBt.setFocusable(false);
-      titlePnl.add(Box.createHorizontalGlue());
-      titlePnl.add(closeBt);
-      pnl.setBorder(Constants.GRAY_BORDER);
-      pnl.add(titlePnl, BorderLayout.NORTH);
    }
 }
    
