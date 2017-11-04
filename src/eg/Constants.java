56d7247851210fbbc5a5f7be66bf7ee19c5b6b52
Malte Bussiek <m.bussiek@web.de>
2017-11-04 22:23:44 +0100

    added contstant for bars

--- src/eg/Constants.java
+++ src/eg/Constants.java
@@ -8,6 +8,7 @@ import java.awt.Toolkit;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
 
 /**
  * Holds different static values
@@ -16,7 +17,6 @@ public class Constants {
 
   //
   // Screen parameters
-  //
 
   /**
    * The screen size */
@@ -33,32 +33,41 @@ public class Constants {
   public final static double SCREEN_RES_RATIO = SCREEN_RES / 72.0;
 
    //
-   // Colors
+   // Sizes
+   
+   /**
+    * The integer used for bars */
+   public final static int BAR_HEIGHT = scaledSize(16);
+  
    //
+   // Colors
 
    /**
     * The gray color */
    public final static Color GRAY = new Color(100, 100, 100);
+   
+   /**
+    * The ligher gray color */
+   public final static Color LIGHT_GRAY = new Color(200, 200, 200);
 
    //
    // Borders
-   //
 
    /**
     * The line border with dark gray color */
    public final static Border GRAY_BORDER = new LineBorder(GRAY, 1);
-   
-   /**
-    * The empty border with thickness of 3 */
-   public final static Border EMPTY_BORDER_3 = new EmptyBorder(3, 3, 3, 3); 
 
    /**
     * The empty border with thickness of 5 pt */
    public final static Border EMPTY_BORDER = new EmptyBorder(5, 5, 5, 5);
+   
+   /**
+    * The <code>MatteBorder</code> with inset of 1 pt in light gray at the top */
+   public final static Border MATTE_TOP = new MatteBorder(1, 0, 0, 0,
+         Constants.LIGHT_GRAY);
 
    //
    // Fonts
-   //
 
    /**
     * The font sans-serif, plain, size 9 pt (scaled) */
@@ -76,10 +85,16 @@ public class Constants {
     * The font consolas, plain, size 9 pt (scaled) */
    public final static Font CONSOLAS_PLAIN_9
          = new Font("Consolas", Font.PLAIN, scaledSize(9.0));
+         
+   //
+   // Strings
+   
+   /**
+    * The system's line separator */
+   public final static String LINE_SEP = System.lineSeparator();
 
     //
     // private
-    //
 
     private static int scaledSize(double size) {
        return (int) (Math.round(size * SCREEN_RES_RATIO));
