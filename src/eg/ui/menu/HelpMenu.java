a98e1b4025e863bb8dbacf491ecd82059c6340d9
Malte Bussiek <m.bussiek@web.de>
2017-11-19 21:19:56 +0100

    add item to open docu website

--- src/eg/ui/menu/HelpMenu.java
+++ src/eg/ui/menu/HelpMenu.java
@@ -3,21 +3,28 @@ package eg.ui.menu;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 
+import eg.ui.Help;
+
 /**
  * The menu for help actions
  * <p>Created in {@link MenuBar}
  */
 class HelpMenu {
    
+   private final Help h = new Help();
+   
    private final JMenu     menu        = new JMenu("?");
+   private final JMenuItem showHelpItm = new JMenuItem("Open help site");
+   private final JMenuItem showDocuItm = new JMenuItem("Open docu site");
    private final JMenuItem aboutItm    = new JMenuItem("About Eadgyth");
-   private final JMenuItem showHelpItm = new JMenuItem("Show help");
    
    public HelpMenu() {
-      menu.add(aboutItm);
       menu.add(showHelpItm);
+      menu.add(showDocuItm);
+      menu.add(aboutItm);
       aboutItm.addActionListener(e -> new eg.ui.InfoWin());      
-      showHelpItm.addActionListener(e -> new eg.ui.Help());  
+      showHelpItm.addActionListener(e -> h.showHelpSite());
+      showDocuItm.addActionListener(e -> h.showDocuSite());
    }
    
    public JMenu getMenu() {
