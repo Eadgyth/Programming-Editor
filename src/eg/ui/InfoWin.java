5b14fed77310f6b1f1ace68f86c1df2de38f5a0c
Malte Bussiek <m.bussiek@web.de>
2017-11-19 21:19:35 +0100

    add website in info

--- src/eg/ui/InfoWin.java
+++ src/eg/ui/InfoWin.java
@@ -11,51 +11,54 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 
+//--Eadgyth--/
+import eg.Constants;
+
 
 /**
  * A frame that shows info about Eadgyth
  */
 public class InfoWin {
 
-   private final static Font TITLE_FONT = new Font("Verdana", Font.BOLD, 14);
    private final static String INFO
          = "<html>"
          + "Version 1.0 beta<br>"
-         + "Malte Bussiek, m.bussiek@web.de"
+         + "Malte Bussiek<br>"
+         + "https://eadgyth.github.io/Programming-Editor/"
          + "</html>";
 
-   private final JFrame frame  = new JFrame("About Eadgyth");
+   private final JFrame frame  = new JFrame();
    private final JPanel okButton = new JPanel(new FlowLayout());
    private final JPanel text     = new JPanel(new GridLayout(2, 1));
    private final JPanel combine  = new JPanel(new BorderLayout());
-   private final JButton closeBt = new JButton("OK");
-   private final JLabel titleLb  = new JLabel("Eadgyth");
+   private final JButton okBt = new JButton("OK");
+   private final JLabel titleLb  = new JLabel("Eadgyth Programming-Editor");
    private final JLabel infoLb   = new JLabel(INFO);
 
    public InfoWin() {  
-      titleLb.setFont(TITLE_FONT);
-      closeBt.setFocusPainted(false);
+      titleLb.setFont(Constants.SANSSERIF_BOLD_11);
+      infoLb.setFont(Constants.SANSSERIF_PLAIN_9);
+      okBt.setFocusPainted(false);
+      okBt.addActionListener(e -> frame.setVisible(false));  
 
-      okButton.add(closeBt);
+      okButton.add(okBt);
       
       text.setBackground(Color.white);
       text.setBorder(eg.Constants.EMPTY_BORDER);
       text.add(titleLb);
       text.add(infoLb);
 
-      combine.setBorder(eg.Constants.EMPTY_BORDER);
+      combine.setBorder(Constants.EMPTY_BORDER);
       combine.add(text, BorderLayout.CENTER);
       combine.add(okButton, BorderLayout.SOUTH);
             
       frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-      frame.pack();
-      frame.setResizable(false);
-      frame.setLocation(650, 100);
-      frame.setSize(300, 150);
-      frame.setContentPane(combine);
       frame.setIconImage(IconFiles.EADGYTH_ICON_16.getImage());
+      frame.setLocation(650, 100);
+      frame.setTitle("About");
+      frame.setContentPane(combine);
+      frame.setResizable(false);
+      frame.setSize(eg.utils.ScreenParams.scaledDimension(300, 150));
       frame.setVisible(true);
-
-      closeBt.addActionListener(e -> frame.setVisible(false));  
    }
 }
