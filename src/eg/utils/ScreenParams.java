c72da7626f65784b20952db5b0be442ad0b53f5c
Malte Bussiek <m.bussiek@web.de>
2017-11-21 20:12:27 +0100

    modify scaling when java 9 is used

--- src/eg/utils/ScreenParams.java
+++ src/eg/utils/ScreenParams.java
@@ -4,9 +4,17 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 
 /**
- * Static methods to obtain quantities that depend on the screen resolution
+ * Static constants and methods to obtain quantities that depend on the screen size and
+ * resolution
  */
 public class ScreenParams {
+   
+   public final static boolean IS_WINDOWS
+         = System.getProperty("os.name").toLowerCase().contains("win");
+   private final static boolean IS_JAVA_9 = System.getProperty("java.version").startsWith("9");  
+   private final static int SCREEN_RES
+         = Toolkit.getDefaultToolkit().getScreenResolution();
+   private final static double SCREEN_RES_RATIO = SCREEN_RES / 72.0;
    
   /**
    * The screen size */
@@ -28,26 +36,26 @@ public class ScreenParams {
    }
   
    /**
-    * Returns an integer that is the rounded product of the specified
-    * size and the ratio between the screen resolution and the resolution
-    * assumed by Java's Graphics2D
+    * Returns an integer that is the rounded product of the specified size and
+    * the resolution ratio.<br>
+    * This ratio is the screen resolution divided by the resolution assumed by Java's
+    * Graphics2D (72 dpi). However, when the program is run using Java 9 the resolution
+    * ratio is constantly 96/72 when the operating system is Windows and 1 otherwise.
     *
     * @param size  the size
     * @return  the rounded rescaled size
     */
    public static int scaledSize(double size) {
       if (IS_JAVA_9) {
-         return (int) (Math.round(size * 96/72));
+         if (IS_WINDOWS) {
+            return (int) (Math.round(size * 96/72));
+         }
+         else {
+            return (int) size;
+         }
       }
       else {
          return (int) (Math.round(size * SCREEN_RES_RATIO));
       }
    }
-   
-   public final static boolean IS_JAVA_9 = System.getProperty("java.version").startsWith("9");
-   
-   private final static int SCREEN_RES
-        = Toolkit.getDefaultToolkit().getScreenResolution();
-
-   private final static double SCREEN_RES_RATIO = SCREEN_RES / 72.0;
 }
