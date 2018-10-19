9d6b809bbc6d16ac1f18e5b9e9640a243ad94340
Malte Bussiek <m.bussiek@web.de>
2018-10-19 22:24:54 +0200

    add a very small font size

--- src/eg/ui/FontSettingWin.java
+++ src/eg/ui/FontSettingWin.java
@@ -25,7 +25,7 @@ import eg.Constants;
 public class FontSettingWin {
 
    private final static String[] FONT_SIZES = {
-     "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"
+     "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"
    };
 
    private final JFrame frame = new JFrame("Font");
