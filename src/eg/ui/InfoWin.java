53c92a7436ffb3492e28a9cb81ab8aa03cde3cf3
Malte Bussiek <m.bussiek@web.de>
2018-11-25 22:34:25 +0100

    change version note

--- src/eg/ui/InfoWin.java
+++ src/eg/ui/InfoWin.java
@@ -19,7 +19,7 @@ public class InfoWin {
 
    private final static String INFO
          = "<html>"
-         + "Version 1.0.6.3<br>"
+         + "Version to test since 1.0.6.3<br>"
          + "Malte Bussiek<br>"
          + "https://eadgyth.github.io/Programming-Editor"
          + "</html>";
