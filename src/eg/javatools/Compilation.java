6322114671c87b73e8a28bd4967d77951cbb1e2e
Malte Bussiek <m.bussiek@web.de>
2019-01-29 11:22:12 +0100

    Update Compilation.java

--- src/eg/javatools/Compilation.java
+++ src/eg/javatools/Compilation.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 import java.util.List;
-//import java.util.Arrays;
 import java.util.ArrayList;
 
 import static java.nio.file.StandardCopyOption.*;
