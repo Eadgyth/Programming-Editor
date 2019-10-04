20521f0a364c57dba7eae4013509862566bbbacc
Malte Bussiek <m.bussiek@web.de>
2019-10-04 20:38:34 +0200

    update help and javadoc

---
+++ docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"eg"},{"l":"eg.console"},{"l":"eg.document"},{"l":"eg.document.styledtext"},{"l":"eg.edittools"},{"l":"eg.javatools"},{"l":"eg.projects"},{"l":"eg.syntax"},{"l":"eg.ui"},{"l":"eg.ui.filetree"},{"l":"eg.ui.menu"},{"l":"eg.ui.tabpane"},{"l":"eg.utils"}]
