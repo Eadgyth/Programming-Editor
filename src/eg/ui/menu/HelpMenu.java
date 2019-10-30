a8935904489759d17b864f6f862f479d4c1524c6
Malte Bussiek <m.bussiek@web.de>
2019-10-30 21:42:20 +0100

    change link to help site and remove link to web page (which is now Readme.md)

--- src/eg/ui/menu/HelpMenu.java
+++ src/eg/ui/menu/HelpMenu.java
@@ -14,32 +14,24 @@ import java.net.URISyntaxException;
  * The menu for help actions
  */
 class HelpMenu {
-   
+
    private final JMenu     menu        = new JMenu("?");
    private final JMenuItem showHelpItm = new JMenuItem("Open help site");
-   private final JMenuItem showDocuItm = new JMenuItem("Open docu site");
    private final JMenuItem aboutItm    = new JMenuItem("About");
-   
+
    public HelpMenu() {
       menu.add(showHelpItm);
-      menu.add(showDocuItm);
-      menu.addSeparator();
       menu.add(aboutItm);
-      aboutItm.addActionListener(e -> new eg.ui.InfoWin());      
+      aboutItm.addActionListener(e -> new eg.ui.InfoWin());
       showHelpItm.addActionListener(e -> showHelpSite());
-      showDocuItm.addActionListener(e -> showDocuSite());
    }
-   
+
    public JMenu getMenu() {
       return menu;
    }
 
    private void showHelpSite() {
-      openWebSite("https://eadgyth.github.io/Programming-Editor/help/help.html");
-   }
-
-   private void showDocuSite() {
-      openWebSite("https://eadgyth.github.io/Programming-Editor/");
+      openWebSite("https://eadgyth.github.io/Programming-Editor/docs/help/help.html");
    }
 
    private void openWebSite(String url) {
