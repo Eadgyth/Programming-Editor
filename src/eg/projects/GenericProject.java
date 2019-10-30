9bb847b8b0b57c279f9e4e3f7d2eeacedfcc509b
Malte Bussiek <m.bussiek@web.de>
2019-10-30 10:34:30 +0100

    add to define a system command in "generic" project

--- src/eg/projects/GenericProject.java
+++ src/eg/projects/GenericProject.java
@@ -1,29 +1,60 @@
 package eg.projects;
 
-import eg.ui.ProjectActionsUpdate;
+//--Eadgyth--/
+import eg.TaskRunner;
+import eg.utils.Dialogs;
+import eg.Projects.ProjectActionsUpdate;
 
 /**
- * Represents a project that is only defined by its directory
+ * Represents a project to run a custom system command
  */
 public final class GenericProject extends AbstractProject implements ProjectCommands {
+   
+   private final TaskRunner runner;
+   
+   boolean isCmd;
 
-   public GenericProject() {
+   public GenericProject(TaskRunner runner) {
       super(ProjectTypes.GENERIC, false, null);
+      this.runner = runner;
    }
 
    @Override
    public void buildSettingsWindow() {
-      inputOptions.buildWindow();
+      inputOptions.addCustomCommandInput()
+            .buildWindow();
    }
 
    @Override
    public void enable(ProjectActionsUpdate update) {
-      // does nothing
+      update.enableRun(false);
    }
 
    /**
     * Not implemented
     */
    @Override
-   protected void setCommandParameters() {}
+   protected void setCommandParameters() {
+      isCmd = !customRunCmd().isEmpty();
+   }
+   
+   @Override
+   public void run() {
+     if (isCmdDefined()) {
+         runner.runSystemCommand(customRunCmd());
+      }
+   }
+   
+   //
+   //--private--/
+   //
+   
+   private boolean isCmdDefined() {
+      boolean isCmd = !customRunCmd().isEmpty();
+      if (!isCmd) {
+         Dialogs.errorMessage(
+            "A system command is not specified.", null);
+      }
+      return isCmd;
+   }
 }
