e695daeaf84e7818864cab583a48086151bf548c
Malte Bussiek <m.bussiek@web.de>
2023-07-20 11:50:06 +0200

    revert last update which was incorrect (sorry!)

--- src/eg/ui/EditArea.java
+++ src/eg/ui/EditArea.java
@@ -288,7 +288,7 @@ public final class EditArea {
          String kind = elem.getName();
          if (kind != null) {
             if (kind.equals(AbstractDocument.ContentElementName)) {
-               return new LabelView(elem);
+               return new WrapLabelView(elem);
             } else if (kind.equals(AbstractDocument.ParagraphElementName)) {
                //
                // Using WrapParagraphView is (for some reason)
